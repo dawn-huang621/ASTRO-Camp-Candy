dec8dd764d6ea43f09c87d430dd133dc68d80277
q2379685 <q2379685@gmail.com>
2023-06-25 23:11:11 +0800

    002 跟 008 練習題卡關

---
+++ storehouse.js
@@ -0,0 +1,9 @@
+// storehouse倉庫
+
+// 002
+            // if(this==this.toUpperCase() ){
+            //     word.map(str=>
+            //     upperCaseword.push(str.toUpperCase())
+            // )} else { return false;
+            // }
+            // console.log(upperCaseword)
