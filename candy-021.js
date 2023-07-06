9f803b16428e1828947029d2f2458e2e17b784c3
q2379685 <q2379685@gmail.com>
2023-07-06 22:03:08 +0800

    今天未完成事項

---
+++ candy-021.js
@@ -0,0 +1,35 @@
+// 編號：CANDY-021
+// 程式語言：JavaScript
+// 題目：實作 Stack 資料結構
+
+class Stack {
+    constructor(){
+        this.data = []
+    }
+    push (el) {
+        this.data.push(el)
+    }
+
+    pop () {
+        if(this.data.length === 0){
+            return null
+        }
+        return this.data.pop()
+    }
+
+    get size(){
+        return this.data.length
+    }
+  }
+  
+  const stack = new Stack()
+  stack.push(123)
+  stack.push(456)
+  stack.push()
+  console.log(stack.size) // 印出 2
+  
+  const item = stack.pop() // 取出元素
+  console.log(item) // 印出 456
+  
+  stack.pop() // 繼續取出元素
+  console.log(stack.size) // 印出 0
